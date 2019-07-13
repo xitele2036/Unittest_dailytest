118eb7ffee989a95a4c2476855323aecd6e9ad58
Administrator <cji@creatcomm.com>
2019-07-13 17:50:47 +0800

    完善日志记录

--- common/readconfig.py
+++ common/readconfig.py
@@ -3,11 +3,12 @@ import os
 from pathlib import Path
 
 class ReadConfig(object):
-    def __init__(self):
+    def __init__(self,filename):
 
-        self.conf_dir = os.path.realpath('..\config\config.ini')
+        # self.conf_dir = os.path.realpath('..\config\config.ini')
+        # self.conf_dir = os.path.realpath('..\config\config.ini')
         self.cf = configparser.ConfigParser()
-        self.cf.read(self.conf_dir)
+        self.cf.read(filename)
 
     def getConfVal(self,section,option):
         return self.cf.get(section,option)
